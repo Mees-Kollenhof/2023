e3d5060397823b09f21b8d4a02d1ec54ce24769c
Mees Kollenhof <152327@csgaugustinus.nl>
2023-03-18 16:31:40 +0000

    jos

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,10 +1,27 @@
-var aantalRijenRaster = 6;
-var aantalKolommenRaster = 9;
-var celGrootte;
+var raster = {
+  aantalRijen: 6,
+  aantalKolommen: 9,
+  celGrootte: null,
+  
+  berekenCelGrootte() {
+    this.celGrootte = width / this.aantalKolommen;
+  },
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
 
 var animatie = [];
-var aantalBeeldjes = 4;
-var nummer = 4;
+var aantalBeeldjes = 6;
+var nummer = 3;
 var frame;
 var xJos = 400;
 var yJos = 300;
@@ -12,7 +29,7 @@ var yJos = 300;
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
   for (var b = 0;b < aantalBeeldjes;b++) {
-    frame = loadImage("spritesheet.png");
+    frame = loadImage("images/sprites/Jos100px/Jos_"+b+".png");
     animatie.push(frame);
   }
 }
@@ -21,44 +38,32 @@ function setup() {
   canvas = createCanvas(900,600);
   canvas.parent('processing');
   frameRate(10);
-  celGrootte = width / aantalKolommenRaster;
+  raster.berekenCelGrootte();
 }
 
 function draw() {
   background(brug);
-  tekenRaster();
+  raster.teken();
 
   if (keyIsDown(LEFT_ARROW)) {
-    xJos -= celGrootte;
-    nummer = 3;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    xJos+=celGrootte;
-    nummer=4;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    yJos -= celGrootte;
-    nummer = 1;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    yJos += celGrootte;
+    xJos -= raster.celGrootte;
     nummer = 2;
   }
-  
-  xJos = constrain(xJos,0,width - celGrootte);
-  yJos = constrain(yJos,0,height - celGrootte);
-  
-  image(animatie[nummer],xJos,yJos,celGrootte,celGrootte);
-}
-
-function tekenRaster() {
-  push();
-  noFill();
-  stroke('grey');
-  for (var rij = 0;rij < aantalRijenRaster;rij++) {
-    for (var kolom = 0;kolom < aantalKolommenRaster;kolom++) {
-      rect(kolom*celGrootte,rij*celGrootte,celGrootte,celGrootte);
-    }
+  if (keyIsDown(RIGHT_ARROW)) {
+    xJos += raster.celGrootte;
+    nummer = 1;
   }
-  pop();
+  if (keyIsDown(UP_ARROW)) {
+    yJos -= raster.celGrootte;
+    nummer = 4;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yJos += raster.celGrootte;
+    nummer = 5;
+  }
+  
+  xJos = constrain(xJos,0,width - raster.celGrootte);
+  yJos = constrain(yJos,0,height - raster.celGrootte);
+  
+  image(animatie[nummer],xJos,yJos,raster.celGrootte,raster.celGrootte);
 }
